cea4038f295774016307b85f7488250c263613eb
irenelynnq <48982694+irenelynnq@users.noreply.github.com>
2019-10-21 18:35:58 +0900

    trash script

---
+++ RecyclingGame/Assets/Scripts/Trash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public int id;
+    public string name;
+    public bool is_answer;
+
+    //플레이어와 충돌시 게임화면에서 사라집니다
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "TrashCollector")
+        {
+            gameObject.SetActive(false);
+            if (is_answer == true) gameObject.GetComponent<TrashCollector>().collected_right.Add(this);
+            else gameObject.GetComponent<TrashCollector>().collected_wrong.Add(this);
+        }
+    }
+}
---
+++ RecyclingGame/Assets/Scripts/TrashCollector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCollector : MonoBehaviour
+{
+    public List<Trash> collected_right;
+    public List<Trash> collected_wrong;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
